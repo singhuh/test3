caccb5f98fc87556fe7bb100025950f89fd95200
INTEL <INTEL@DESKTOP-OKKS9M9>
2020-12-09 15:25:03 -0500

    Add project files.

---
+++ Areas/Identity/Models/AppUser.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace test3.Areas.Identity.Models
+{
+    public class AppUser:IdentityUser
+    {
+        [Key]
+        [Required(ErrorMessage = "M Number is required")]
+        [RegularExpression(@"\d{8}")]
+        [Display(Name = "M Number")]
+        public int MNumber { get; set; }
+
+        public string Name { get; set; }
+        public string E_mail { get; set; }
+        [Display(Name = "Personal LinkedIn Url")]
+        public string LinkedIn { get; set; }
+        public string Resume { get; set; }
+        public byte[] Photo { get; set; }
+
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using test3.Areas.Identity.Models;
+
+namespace test3.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<AppUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
