ac15fb0668b8eca37bfcb14512e066e482a18138
INTEL <INTEL@DESKTOP-OKKS9M9>
2020-12-09 17:43:35 -0500

    Update photo

--- Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
+++ Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
@@ -14,6 +14,8 @@ namespace test3.Areas.Identity.Pages.Account.Manage
 
         public static string ChangePassword => "ChangePassword";
 
+        public static string ChangePhoto => "ChangePhoto";
+
         public static string DownloadPersonalData => "DownloadPersonalData";
 
         public static string DeletePersonalData => "DeletePersonalData";
@@ -29,6 +31,8 @@ namespace test3.Areas.Identity.Pages.Account.Manage
         public static string EmailNavClass(ViewContext viewContext) => PageNavClass(viewContext, Email);
 
         public static string ChangePasswordNavClass(ViewContext viewContext) => PageNavClass(viewContext, ChangePassword);
+
+        public static string ChangePhotoNavClass(ViewContext viewContext) => PageNavClass(viewContext, ChangePhoto);
 
         public static string DownloadPersonalDataNavClass(ViewContext viewContext) => PageNavClass(viewContext, DownloadPersonalData);
 
