bea08bde7a6b9281744d05f34143b247f61541c6
INTEL <INTEL@DESKTOP-OKKS9M9>
2020-12-09 23:36:41 -0500

    PDF functionality; view in browser

---
+++ Controllers/ResumesController.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using test3.Data;
+using test3.Models;
+
+namespace test3.Controllers
+{
+    [Authorize]
+    public class ResumesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ResumesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Resumes
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Resumes.ToListAsync());
+        }
+
+        // GET: Resumes/Details/5
+        public async Task<IActionResult> Details(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var resume = await _context.Resumes
+                .FirstOrDefaultAsync(m => m.ResumeID == id);
+            if (resume == null)
+            {
+                return NotFound();
+            }
+
+            return View(resume);
+        }
+
+        // GET: Resumes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Resumes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Resume resume, IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+            {
+                return View(resume);
+            }
+
+            var ext = Path.GetExtension(file.FileName);
+            var newFileName = "resumes" + DateTime.Now.ToString("yymm") + ext;
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Resumes", newFileName);
+
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            resume.ResumeFile = "/Resumes/" + newFileName;
+            _context.Add(resume);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index", "Resumes");
+/*            if (ModelState.IsValid)
+            {
+                _context.Add(resume);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(resume);*/
+        }
+
+        // GET: Resumes/Edit/5
+        public async Task<IActionResult> Edit(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var resume = await _context.Resumes.FindAsync(id);
+            if (resume == null)
+            {
+                return NotFound();
+            }
+            return View(resume);
+        }
+
+        // POST: Resumes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(long id, [Bind("ResumeID,UserId,Description,ResumeFile")] Resume resume)
+        {
+            if (id != resume.ResumeID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(resume);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ResumeExists(resume.ResumeID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(resume);
+        }
+
+        // GET: Resumes/Delete/5
+        public async Task<IActionResult> Delete(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var resume = await _context.Resumes
+                .FirstOrDefaultAsync(m => m.ResumeID == id);
+            if (resume == null)
+            {
+                return NotFound();
+            }
+
+            return View(resume);
+        }
+
+        // POST: Resumes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(long id)
+        {
+            var resume = await _context.Resumes.FindAsync(id);
+
+            var RelativePath = resume.ResumeFile;
+            var AbsolutePath = "wwwroot" + RelativePath;
+
+            if (System.IO.File.Exists(AbsolutePath))
+            {
+                System.IO.File.Delete(AbsolutePath);
+            }
+
+            _context.Resumes.Remove(resume);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ResumeExists(long id)
+        {
+            return _context.Resumes.Any(e => e.ResumeID == id);
+        }
+    }
+}
